49a2d11e63302c5688311c395d83ba0af5327e44
Alexander Ilyin <alexander@ilyin.eu>
2025-01-30 01:23:35 +0000

    feat(devcontainer): Improve Dockerfile, add office repo
    - Update Dockerfile to improve efficiency.
    - Added a new github repository for office.

--- modules/github/main.tf
+++ modules/github/main.tf
@@ -17,7 +17,7 @@ resource "github_organization_settings" "cloud-9-aviation" {
 resource "github_repository" "cloud-9-aviation_github_io" {
   # TODO: Try to use 'github_organization_settings' resource.
   name        = "${data.github_organization.cloud-9-aviation.orgname}.github.io"
-  description = "The True Hitchhiker's Guide to the Galaxy."
+  description = "https://cloud-9-aviation.github.io/"
 
   allow_auto_merge       = true
   delete_branch_on_merge = true
@@ -61,4 +61,18 @@ resource "github_branch_protection" "terraform" {
   }
 
   enforce_admins = true
+}
+
+resource "github_repository" "office" {
+  name        = "office"
+  description = "virtual office"
+  visibility  = "private"
+
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+  has_issues             = true
+  has_projects           = true
+  vulnerability_alerts   = false
+  auto_init              = true
+  has_downloads          = false
 }
